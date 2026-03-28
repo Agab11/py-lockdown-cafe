038ca9542e5a646109c0c0e637383ba02da8acd5
Agnieszka Barcik <aga.barcik11@gmail.com>
2026-03-28 18:34:28 +0100

    fix reviews comments

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.cafe import Cafe
-from app.errors import VaccineError, git add .NotWearingMaskError
+from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list[dict], cafe: Cafe) -> str:
